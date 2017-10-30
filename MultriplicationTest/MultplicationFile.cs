71b8041c6d9e464fb518818d997111374d0a75e6
Jahurul Islam <jahurul1983@gmail.com>
2017-10-30 09:17:51 +0100

    Startexercise

---
+++ MultriplicationTest/MultplicationFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultriplicationTest
+{
+    [TestClass]
+    public class MultplicationFile
+    {
+        [TestMethod]
+        public void Multriplication()
+        {
+        }
+    }
+}
---
+++ MultriplicationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MultriplicationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultriplicationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0ce015f-9f3c-4c6f-8c24-f2390b59a063")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
